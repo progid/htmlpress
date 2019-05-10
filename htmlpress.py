a621d12e0175a6ec85aa12fbb97fcbcd83efb176
Igor <igor.terletskiy.96@gmail.com>
2019-05-10 19:49:39 +0300

    Select scripts and styles to the another section

--- htmlpress.py
+++ htmlpress.py
@@ -7,15 +7,36 @@ __author__ = "Igor Terletskiy"
 __version__ = "0.0.1"
 __license__ = "MIT"
 
-def optimiseHtmlHeadPart(headcontent):
-	
-
-def optimiseHtmlBodyPart(bodypart):
-	
-
 def simplifyDict(rawDict):
-	
+	log(rawDict)
 
+def optimiseHtmlHeadPart(headpart):
+	links = re.findall(r'<link[^>]*>', headpart)
+	preparedHeadpart = re.sub(r'<link[^>]*>', '', headpart)
+	startStyleTags = re.finditer(r'<style[^>]*>', headpart)
+	result = links
+	for match in startStyleTags:
+		startPos, endPos = match.span()
+		styleTag = headpart[startPos:startPos + headpart[startPos:].find('</style>')]
+		result.append(styleTag)
+	return result
+	
+def optimiseHtmlBodyPart(bodypart):
+	connectScriptTags = re.findall(r'<script[^>]*></script>', bodypart)
+	preparedBodypart = re.sub(r'<script[^>]*></script>', '', bodypart)
+	startScriptTags = re.finditer(r'<script[^>]*>', bodypart)
+	result = connectScriptTags
+	for match in startScriptTags:
+		startPos, endPos = match.span()
+		scriptTag = bodypart[startPos:startPos + bodypart[startPos:].find('</script>')]
+		result.append(scriptTag)
+	return result
+
+
+def optimiseHtmlDict(htmldict):
+	head = optimiseHtmlHeadPart(htmldict['head'])
+	body = optimiseHtmlBodyPart(htmldict['body'])
+	return { 'head': head, 'body': body }
 
 def optimiseHtmlsDict(htmlsdict):
 	result = dict()
@@ -23,9 +44,6 @@ def optimiseHtmlsDict(htmlsdict):
 		htmldict = htmlsdict[item]
 		result[item] = optimiseHtmlDict(htmldict)
 	return simplifyDict(result)
-
-def optimiseHtmlDict(htmldict):
-
 
 def parseHTMLHeadPart(content):
 	start = content.find('<head>') + len('<head>')
@@ -76,15 +94,13 @@ def makeHTMLParsing(htmlFilepaths):
 def getArgsData():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--input', nargs='+', help='input help')
-	parser.add_argument('--singlefile', action='store_true', help='singlefile help')
-	parser.add_argument('--multifiles', action='store_true', help='multifiles help')
+	parser.add_argument('--singlefile', nargs='?', help='singlefile help')
 	
 	return parser.parse_args()
 
 def main():
 	args = getArgsData()
 	preparedData = makeHTMLParsing(args.input)
-	print(args);
 
 if __name__ == "__main__":
 	main()
